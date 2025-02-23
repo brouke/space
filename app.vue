ba699518ba4ee0b2fc1341b19eed13218af71254
Svoevolina <urik197250@gmail.com>
2025-02-24 03:40:45 +0400

    fix

--- app.vue
+++ app.vue
@@ -1,15 +1,24 @@
 <template>
-  <Button 
+  <div class="head">
+    <div class="head_logo">
+     <a href="/"><h1>Logo</h1></a>
+     <div class="head_menu">
+      <a href="Info.vue"><h1>О Нас</h1></a>
+      <a href=""><button class="head_menu_but"><h1>Войти</h1></button></a>
+     </div>
+    </div>
+    <div class="banner">
+      
+      <img class="banner_img" src="assets/scss/img/81e9615feb8a0f4cee1aa91c6986b622.jpg" alt="">
+      <div class="banner_inf"><h1>Аренда помещений
+        в торговом центре. </h1>
+        <button class="banner_inf_but">Смотреть помещения</button>
+    </div></div>
+  </div> 
+   <!-- <Button 
         placeholder="добавить"
         :className="'btn__add-button'"
-        />
-  <div class="head">
-    <div class="head_button">
-      <button></button>
-      <div class="head_input">
-      </div>
-    </div>
-  </div>  
+        />  -->
 </template>
 <style >
 @import '@/assets/scss/index.scss';
