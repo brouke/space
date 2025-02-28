ece78cebe6a313e19cdff2af239ab31291240a66
Svoevolina <urik197250@gmail.com>
2025-03-01 03:52:25 +0400

    fix

---
+++ pages/users.vue
@@ -0,0 +1,27 @@
+<template>
+    <Header></Header>
+    <div class="title">Все пользователи</div>
+
+    <div class="user">
+        <div class="user_info1">
+            <div class="user_info1_title">Данные пользователя:</div>
+            <div class="user_info1_desc">
+                Login: <br>
+                Password: <br>
+                Имя: <br>
+                Фамилия: <br>
+            </div>
+        </div>
+        <div class="user_info1">
+            <div class="user_info1_title">Дополнительные данные:</div>
+            <div class="user_info1_desc">
+                Имя компании: <br>
+                Адрес: <br>
+                Телефон: <br>
+                Реквизиты: <br>
+                Контактное лицо: <br>
+            </div>   
+        </div>
+        
+    </div>
+</template>
