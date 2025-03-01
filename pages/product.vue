45df1008d5f729c4a7b5a7e6ad9f8ee14448212c
Svoevolina <urik197250@gmail.com>
2025-03-01 07:33:30 +0400

    fix

--- pages/product.vue
+++ pages/product.vue
@@ -1,3 +1,23 @@
 <template>
-    
+    <Header></Header>
+
+    <div class="prod">
+        <div class="prod_info">
+            <div class="prod_info_img"></div>
+            <div class="prod_info_desc">
+                <div class="prod_info_desc_title">Имя + Cтоимость</div>
+                <div class="prod_info_desc_inf">Описание</div>
+                <div class="prod_info_desc_stat">
+                    <li>Площадь:</li>
+                    <li>Этаж:</li>
+                    <li>Кондиционер:</li>
+                    <li>Стоимость:</li>
+                </div>
+                <Button 
+                        placeholder="Арендовать"
+                        :className="'btn_buy-button'"
+                        />
+            </div>
+        </div>
+    </div>
 </template>
